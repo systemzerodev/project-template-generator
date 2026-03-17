77e64e4baecbd0fa12eef6e831a3a11da13171d2
systemzerodev <stevanusdewangga120991@gmail.com>
2026-03-17 15:18:38 +0700

    feat: add project generator CLI tool with Tkinter UI

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="project-generator",
-    version="0.1",
+    version="0.1.0",
+    description="CLI tool to generate project templates with UI",
+    author="FullstackDev",
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        "": ["template/*"],
+    },
     entry_points={
         "console_scripts": [
             "create-project=project_generator.ui:run",
         ],
     },
+    python_requires=">=3.8",
 )
